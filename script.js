a92dd4b48b1cc73a5f408c3a537e342a15d80fa6
Cocolessice950 <trinhhan545@gmail.com>
2025-02-25 07:24:34 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function login()
+{
+  document.getElementById(compiler).style.display="block"
+  let u = username.value
+  let p = pw.value
+  if (u=="baohan"&& p="123")
+   {
+     window.open()
+     
+    }
+ }
